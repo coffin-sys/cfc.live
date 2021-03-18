f5f8de544cdbd510fc1b5c6242a89a47191a0eff
coffin-sys <80905998+coffin-sys@users.noreply.github.com>
2021-03-18 19:21:00 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/azimjohn/cfc.live/jprq"
+	"github.com/coffin-sys/cfc.live/cfc"
 	"github.com/gorilla/mux"
 	"github.com/labstack/gommon/log"
 	"net/http"
@@ -15,7 +15,7 @@ func main() {
 	flag.StringVar(&baseHost, "host", "cfc.live", "Base Host")
 	flag.Parse()
 
-	j := jprq.New(baseHost)
+	j := cfc.New(baseHost)
 	r := mux.NewRouter()
 	r.HandleFunc("/_ws/", j.WebsocketHandler)
 	r.PathPrefix("/").HandlerFunc(j.HttpHandler)
